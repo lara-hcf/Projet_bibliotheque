170e39a6420bd5dced4c6e6688f814b9ef9a95ea
lara-hcf <lara.hocf@gmail.com>
2024-04-12 10:45:23 +0200

    corriger init v2

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from flask import Flash, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
 
-app= Flask(__name__)
+app = Flask(__name__)   
 
 
 @app.route('/')
