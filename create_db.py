a83d3cfa0031d41862992fbea21c6c3e5a216797
lara-hcf <lara.hocf@gmail.com>
2024-04-12 15:38:09 +0200

    test database

--- create_db.py
+++ create_db.py
@@ -5,6 +5,9 @@ connection= sqlite3.connect('database2.db')
 with open('schemas.sql') as f:
     connection.executescript(f.read())
 
+cur= connection.cursor()
+
+cur.execute("INSERT INTO livres (titre,auteur_prenom, auteur_nom) VALUES (?,?,?)", ('Chez le roi merlin','Arthur','Jean'))
 
 connection.commit()
 connection.close()
